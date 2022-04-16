fd161303cef545dcafda60880c9b789c40699d58
Bishal Halder <vishalh016@gmail.com>
2022-04-16 15:47:57 +0530

    Asp.net MVC practice day1
    first project in MVC Added

---
+++ WEBAPP1/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        //List<Product> l = new List<Product>()
+        //{
+        //    new Product(){ Id = 1, Name ="Dell", Category="Laptop" },
+        //    new Product(){ Id = 2, Name ="Asus", Category="GamingLaptop" },
+        //    new Product(){ Id = 3, Name ="Hp", Category ="PC"}
+        //};
+        static List<Product> AllProducts;
+        static ProductController()
+        {
+            AllProducts = new List<Product>()
+            {
+                new Product(){ Id = 1, Name ="Dell", Category="Laptop" },
+                new Product(){ Id = 2, Name ="Asus", Category="GamingLaptop" },
+                new Product(){ Id = 3, Name ="Hp", Category ="PC"}
+
+            };
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string ShowDate()
+        //{
+        //    DateTime date = DateTime.Now;
+        //    return date.ToString();
+        //}
+        public IActionResult Show() { return View(); }
+        public IActionResult Test() { return View(); }
+        public IActionResult DisplayProduct()
+        {
+            return View(AllProducts);
+        }
+
+    }
+}
